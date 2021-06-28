070976977e8bca6bb7bdedcf5458d0b8c6e7ec58
sophie villalba <villalba.sao@gmail.com>
2021-06-28 08:02:02 +0800

    PALEOZOIC DIALOGUE

--- CORE/Assets/Scripts/LevelManager.cs
+++ CORE/Assets/Scripts/LevelManager.cs
@@ -6,15 +6,23 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public GameObject hide;
+
 
     private void Start()
     {
         pauseMenu.SetActive(false);
+        hide.SetActive(false);
     }
 
     public void TogglePauseMenu()
     {
         pauseMenu.SetActive(!pauseMenu.activeSelf);
+    }
+
+    public void HideJoystick()
+    {
+        hide.SetActive(!hide.activeSelf);
     }
 
     public void ToMenu()
