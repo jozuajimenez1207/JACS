d0818518760ae479aab7599e2b179163efd9e709
PHIEKAMON <sophieangelavillalba@gmail.com>
2021-07-04 07:35:30 +0800

    asset update

---
+++ CORE/Assets/Scripts/Clue.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clue: MonoBehaviour
+{
+    public GameObject uiObject;
+    public GameObject forcefield;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+       
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter (Collider player)
+    {
+        if (player.gameObject.tag =="Player")
+        {
+            uiObject.SetActive(true);
+            
+            StartCoroutine("WaitForSec");
+        }
+    }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(5);
+        uiObject.SetActive(false);
+    }
+}
