2d866403c352ee6d26f395fa4d2af3ccf72c45dc
Angelica-Lacerna <72684985+Angelica-Lacerna@users.noreply.github.com>
2021-07-04 13:56:17 +0800

    update

---
+++ CORE/Assets/Scripts/NextScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    public GameObject Trigger;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Trigger.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("PROTEROZOIC 2 - (CELL)");
+
+        }
+    }
+}
