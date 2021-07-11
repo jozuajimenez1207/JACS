23c6879df54e94b11c4c30035ea171ce81178e13
jozuajimenez1207 <72688245+jozuajimenez1207@users.noreply.github.com>
2021-07-12 01:28:46 +0800

    meteors update

---
+++ CORE/Assets/BreakableAsteroids/Scripts/DestroyFracturedRock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyFracturedRock : MonoBehaviour
+{
+
+    public float time = 3;
+    private bool isFractured;
+
+    void Start()
+    {
+        isFractured = true;
+    }
+
+    void Update()
+    {
+        if (isFractured == true)
+        {
+            time -= Time.deltaTime;
+
+            if (time < 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
