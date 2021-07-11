c5d3b88262cac7c21c64943023418f97cf9b172a
jozuajimenez1207 <72688245+jozuajimenez1207@users.noreply.github.com>
2021-07-12 01:29:40 +0800

    meteors update

---
+++ CORE/Assets/Scripts/MeteorSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorSpawner : MonoBehaviour
+{
+    public GameObject MeteorPrefab;
+
+    public Vector3 center;
+    public Vector3 size;
+
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+
+    void Start()
+    {
+        InvokeRepeating("MeteorSpawn", spawnTime, spawnDelay);
+    }
+
+    public void MeteorSpawn()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x/2, size.x/2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+
+        Instantiate(MeteorPrefab, pos, Quaternion.identity);
+
+        if(stopSpawning)
+        {
+            CancelInvoke("MeteorSpawn");
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(center, size);
+    }
+}
